35d15c4bcfb86948fa11d0e4763693e785fa9c90
DeYiHung <eagle2107821078@gmail.com>
2021-03-05 12:16:47 +0800

    8yee

--- oop/oop/GitTutorial.cpp
+++ oop/oop/GitTutorial.cpp
@@ -14,8 +14,13 @@ bool Guess(int number) {
 	}
 
 
-	if (number == target)return true;
-	return flase;
+	)if (number == target) {
+	std::cout << "Correct!";
+	target = -1;
+	return true;
+	}
+	else std::cout << "Wrong" << std::endl;
+	return false;
 }
 
 int main(array<System::string^>^args)
