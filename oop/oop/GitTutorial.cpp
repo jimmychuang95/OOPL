460ff535dc98e5117b2e0d72a94687f28369499d
DeYiHung <eagle2107821078@gmail.com>
2021-03-05 12:04:21 +0800

    excercise8

--- oop/oop/GitTutorial.cpp
+++ oop/oop/GitTutorial.cpp
@@ -13,7 +13,13 @@ bool Guess(int number) {
 		target = r.Next() % 100 + 1;
 	}
 
-	if (number == target)return true;
+
+	if (number == target) {
+		std::cout << "Correct!";
+		target = -1;
+		return true;
+	}
+	else std::cout << "Wrong" << std::endl;
 	return flase;
 
 }
@@ -23,7 +29,6 @@ int main(array<System::string^>^args)
 {
 	int guess;
 	do {
-		std::cout << "Choose a number between 1 ~ 100 :";
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
